313cf1bd3f3e8a27d78cf74841e44cad50307043
asifalam515 <asibulalamt515@gmail.com>
2022-06-29 17:17:32 +0600

    bs

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-function sum(num1,num2){
-    return num1+num2;
-}
-console.log(sum(34,56))
 
-function binarySearch(){
 
+function binarySearch(arr,target){
+    start=0;
+    end=arr.length-1;
+    while(arr.length>=target ){
+        var mid=start+(end-start)/2;
+        if(arr[mid]==target){
+            return mid;
+        } if(arr[mid]>target){
+            end=mid-1;
+        }
+        if(arr[mid]<target){
+            start=mid+1;
+        }
+    }
 }
+return -1;
+var arr=[3,5,6,87,454]
+var target=5;
+const ans=binarySearch(arr,target);
+console.log(ans)
